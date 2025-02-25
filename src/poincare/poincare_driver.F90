a0a5c569ca8d59070e2e8cfb0f35e8ee0072fc92
Jeremy Lore <lorejd@ornl.gov>
2025-02-25 09:39:00 -0500

    Make poincare use MPI with each node writing to a numbered file

--- src/poincare/poincare_driver.F90
+++ src/poincare/poincare_driver.F90
@@ -6,183 +6,159 @@ Program poincare_driver
   ! Author(s): J.D. Lore 
   !
   Use kind_mod, Only: int32, real64
-  Use poincare_namelist, Only : read_poincare_namelist, num_pts, dphi_line_poincare, &
-       Rstart_poincare, Rend_poincare, Zstart_poincare, Zend_poincare, nsteps, &
-       phistart_deg_poincare, ind_poin
+  Use poincare_namelist
   Use initialize_bfield_div3d, Only : init_bfield
-  Use parallel_mod, Only : rank, nprocs, init_mpi, fin_mpi
+  Use parallel_mod
   Use timing_mod, Only : get_elapsed_time, init_timing  
-  !Use g3d_module, Only : get_psi_bicub
   Use math_routines_mod, Only : rlinspace
   Use fieldline_follow_mod, Only: follow_fieldlines_rzphi
-  !Use util_routines, Only: get_psin_2d
   Use phys_const, Only: pi
   Use setup_bfield_module, Only : bfield
 
   Implicit none
 
-  Integer :: tstart ! Timing variable
-  Integer(int32) :: iocheck, nstart_fl
-  Logical :: verbose = .false.  
-
-  !  Integer(int32) :: i, nstart_fl, nsteps
-  !  Integer(int32) :: iocheck, ind_poin, ierr
-  Real(real64), Allocatable :: r1d(:), z1d(:)
-  Real(real64), Allocatable :: phistart_arr(:), fl_r(:,:), fl_z(:,:), fl_p(:,:)
-  !       psiout(:), psiNout(:), fl_r2(:,:), fl_z2(:,:), fl_p2(:,:)
-  Integer(int32), Allocatable :: ilg(:), fl_ierr(:) !, ilg2(:), fl_ierr2(:)
-
+  Integer(int32), Parameter :: print_progress_every = 1
   
+  Integer :: tstart ! Timing variable
+  Integer(int32) :: iocheck, i, j, offset, num_points_this_slice, ierr, &
+       i_start, i_end, chunk_size, remainder, num_surfs_local, local_idx
+  Logical :: verbose
+  Real(real64) :: Rs, Zs, Ps
+  Real(real64), Allocatable :: r1d(:), z1d(:), phistart_arr(:)
+  Real(real64), Allocatable :: fl_r(:,:), fl_z(:,:), fl_p(:,:)
+  Integer(int32), Allocatable :: ilg(:), fl_ierr(:)
+
+  ! Temporary arrays for each fieldline call
+  Real(real64), Allocatable :: rtemp(:), ztemp(:), ptemp(:)
+  Integer(int32) :: ierrtemp, ilgtemp
+
+  ! For output filename
+  Character(len=256) :: outfilename
+
   !---------------------------------------------------------------------------
   ! 0. Initialization
-  !---------------------------------------------------------------------------  
+  !---------------------------------------------------------------------------
   Call init_mpi
   Call init_timing
-  
-  If (rank .eq. 0) Then
-     call system_clock(tstart)
+
+  verbose = .false.
+  If (rank == 0) Then
+     Call system_clock(tstart)
      verbose = .true.
      Write(*,'(/A)') '-------------------------------------------------------------------------'
-     Write(*,'(a)') " Starting Poincare driver"     
+     Write(*,'(a)') " Starting Poincare driver"
   End If
-  
+
   ! Read namelists
   Call read_poincare_namelist(verbose)
-  
+
+  ! Determine workload distribution
+  chunk_size = num_surfs / nprocs
+  remainder  = mod(num_surfs, nprocs)
+
+  ! Assign loop bounds for each rank
+  i_start = rank * chunk_size + min(rank, remainder) + 1
+  i_end   = i_start + chunk_size - 1
+  If (rank < remainder) i_end = i_end + 1
+
+  ! Number of surfaces this rank is responsible for
+  num_surfs_local = i_end - i_start + 1
+
+  Call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+
   !----------------------------------------------------------
   ! 1. Initialize magnetic field
   !----------------------------------------------------------
   Call init_bfield(verbose)
 
   !----------------------------------------------------------
-  ! 2. Follow fieldlines
+  ! 2. Allocate variables and set start points
   !----------------------------------------------------------
-  Allocate(r1d(num_pts),z1d(num_pts))
-  r1d = rlinspace(Rstart_poincare,Rend_poincare,num_pts)
-  z1d = rlinspace(Zstart_poincare,Zend_poincare,num_pts)
+  Allocate(r1d(num_surfs))
+  Allocate(z1d(num_surfs))
+  Allocate(phistart_arr(num_surfs))
 
-  write(*,*) r1d
-  write(*,*) z1d  
-  
-  
-  nstart_fl = num_pts
-  Allocate(ilg(nstart_fl),fl_ierr(nstart_fl),phistart_arr(nstart_fl))
-  Allocate(fl_r(nstart_fl,nsteps+1),fl_z(nstart_fl,nsteps+1),fl_p(nstart_fl,nsteps+1))
-  fl_r = 0.d0; fl_z = 0.d0; fl_p = 0.d0
-  phistart_arr = phistart_deg_poincare*pi/180.d0
+  r1d(:)          = rlinspace(Rstart_poincare, Rend_poincare, num_surfs)
+  z1d(:)          = rlinspace(Zstart_poincare, Zend_poincare, num_surfs)
+  phistart_arr(:) = phistart_deg_poincare*pi/180.d0
 
-  Call follow_fieldlines_rzphi(bfield,r1d,z1d,phistart_arr,nstart_fl, dphi_line_poincare,nsteps,fl_r,fl_z,fl_p,fl_ierr,ilg)
-  
-  Write(*,*) "Time spent in total: ", get_elapsed_time(tstart), " seconds"
-  Write(*,*) "Time spent per line avg: ", get_elapsed_time(tstart)/nstart_fl, " seconds"
-  
+  ! Local arrays to hold our subset of surfaces
+  Allocate(fl_r(num_surfs_local, nsteps+1))
+  Allocate(fl_z(num_surfs_local, nsteps+1))
+  Allocate(fl_p(num_surfs_local, nsteps+1))
+  Allocate(fl_ierr(num_surfs_local))
+  Allocate(ilg(num_surfs_local))
+
+  ! Allocate temp arrays for the fieldline routine
+  Allocate(rtemp(nsteps+1), ztemp(nsteps+1), ptemp(nsteps+1))
+
+  !----------------------------------------------------------
+  ! 3. Follow fieldlines
+  !----------------------------------------------------------
+  Do i = i_start, i_end
+
+     local_idx = i - i_start + 1
+
+     Rs = r1d(i)
+     Zs = z1d(i)
+     Ps = phistart_arr(i)
+
+     Call follow_fieldlines_rzphi(bfield, Rs, Zs, Ps, dphi_line_poincare, &
+          nsteps, rtemp, ztemp, ptemp, ierrtemp, ilgtemp)
+
+     ! Copy results to our local arrays
+     fl_r(local_idx,:)   = rtemp
+     fl_z(local_idx,:)   = ztemp
+     fl_p(local_idx,:)   = ptemp
+     fl_ierr(local_idx)  = ierrtemp
+     ilg(local_idx)       = ilgtemp
+
+
+     ! Optional progress message every so many surfaces
+     If ( mod(i - i_start, print_progress_every) == 0 ) Then
+        Write(*,*) 'Rank ', rank, ': processed surface ', local_idx, ' of ', num_surfs_local
+     End If
+  End Do
+
+  Call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+  Write(*,*) 'Rank ', rank, ' has completed processing.'
+
+  !----------------------------------------------------------
+  ! 4. Write local data to unique file
+  !----------------------------------------------------------
+  ! surface_data.out.0000 for rank=0, etc.
+  Write(outfilename, '(A,I4.4)') "surface_data.out.", rank
+  Open(unit=99, file=outfilename, status="unknown", form="formatted", iostat=iocheck)
+  If (iocheck /= 0) Then
+     Write(*,*) 'Error opening output file on rank=', rank
+     Stop 'Exiting: I/O Error in poincare_driver.F90'
+  End If
+
+  ! We can write exactly the same "sliced" data structure for this local subset
+  Write(99,*) num_surfs_local, num_slices
+
+  Do i = 0, num_slices-1
+     offset = i*ind_poin/num_slices + 1
+     num_points_this_slice = Int(nsteps + 1 - offset) / ind_poin + 1
+     Write(99,*) dphi_slice*i*180.d0/pi, num_points_this_slice
+
+     ! Now write fl_r, fl_z for each local surface
+     Do j = 1, num_surfs_local
+        Write(99,*) fl_r(j, offset:nsteps+1:ind_poin)
+        Write(99,*) fl_z(j, offset:nsteps+1:ind_poin)
+     End Do
+  End Do
+
+  Close(99)
+
+  !----------------------------------------------------------
+  ! 5. Print timing
+  !----------------------------------------------------------
+  If (rank == 0) Then
+     Write(*,*) "Time spent in total: ", get_elapsed_time(tstart), " seconds"
+     Write(*,*) "Time spent per line avg: ", get_elapsed_time(tstart)/num_surfs, " seconds"
+  End If
+
   Call fin_mpi(.false.)
-  
-  ! If (follow_both_ways) Then
-  !    Allocate(ilg2(nstart_fl),fl_ierr2(nstart_fl))
-  !    Allocate(fl_r2(nstart_fl,nsteps+1),fl_z2(nstart_fl,nsteps+1),fl_p2(nstart_fl,nsteps+1))
-  !    fl_r2 = 0.d0; fl_z2 = 0.d0; fl_p2 = 0.d0 
-  !    Call follow_fieldlines_rzphi(bfield,r1d,z1d,phistart_arr,nstart_fl,-dphi_line,nsteps,fl_r2,fl_z2,fl_p2,fl_ierr2,ilg2)
-  ! Endif
-
-  ! Open(99,file="poincare_output.out",status="unknown",form="formatted",iostat=iocheck)
-  ! If ( iocheck /= 0 ) Then
-  !    Write(*,*) 'Error opening output file'
-  !    Stop 'Exiting: I/O Error in poincare_driver.f90'
-  ! Endif
-  ! Write(99,*) phistart_deg,nstart_fl,nsteps/ind_poin+1
-  ! !Write(*,*) phistart_deg,nstart_fl,nsteps/ind_poin+1
-
-  ! Do i = 1,nstart_fl
-  !    Write(99,*) i
-  !    Write(99,'(6e20.12)') fl_r(i,1:nsteps+1:ind_poin)
-  !    Write(99,'(6e20.12)') fl_z(i,1:nsteps+1:ind_poin)
-  ! Enddo
-  ! Close(99)
-  ! Deallocate(r1d,z1d)
-
-  ! If (follow_both_ways) Then
-  !    Open(99,file="poincare_output2.out",status="unknown",form="formatted",iostat=iocheck)
-  !    If ( iocheck /= 0 ) Then
-  !       Write(*,*) 'Error opening output file'
-  !       Stop 'Exiting: I/O Error in poincare_driver.f90'
-  !    Endif
-  !    Write(99,*) phistart_deg,nstart_fl,nsteps/ind_poin+1
-
-  !    Do i = 1,nstart_fl
-  !       Write(99,*) i
-  !       Write(99,'(6e20.12)') fl_r2(i,1:nsteps+1:ind_poin)
-  !       Write(99,'(6e20.12)') fl_z2(i,1:nsteps+1:ind_poin)
-  !    Enddo
-  !    Close(99)
-  ! Endif
-
-  ! !
-  ! ! Calculate min psi_N
-
-  ! !
-  ! If (calc_psiN_min) Then
-  !    Write(*,*) 'Calculating minimum psi_N'
-  !    Allocate(psiout(nsteps+1),psiNout(nsteps+1))
-
-  !    Open(99,file="psiN_min_output.out",status="unknown",form="formatted",iostat=iocheck)
-  !    If ( iocheck /= 0 ) Then
-  !       Write(*,*) 'Error opening output file'
-  !       Stop 'Exiting: I/O Error in poincare_driver.f90 (2)'
-  !    Endif
-  !    Write(99,*) nstart_fl
-
-  !    Do i = 1,nstart_fl
-
-  !       !Call get_psi_bicub(fl_r(i,:),fl_z(i,:),nsteps+1,psiout,psiNout,ierr)
-  !       psiNout = get_psiN_2d(bfield,fl_r(i,:),fl_z(i,:),nsteps+1,ierr)
-
-  !       Where (psiNout < 1.e-3) psiNout = 1000000.d0
-  !       Write(99,*) Minval(psiNout)
-  !    Enddo
-
-  !    Close(99)
-  !    Deallocate(psiout,psiNout)
-  ! Endif
-
-  ! If (follow_both_ways) Then
-  !    ! Calculate min psi_N
-  !    If (calc_psiN_min) Then
-  !       Write(*,*) 'Calculating minimum psi_N'
-  !       Allocate(psiout(nsteps+1),psiNout(nsteps+1))
-
-  !       Open(99,file="psiN_min_output2.out",status="unknown",form="formatted",iostat=iocheck)
-  !       If ( iocheck /= 0 ) Then
-  !          Write(*,*) 'Error opening output file'
-  !          Stop 'Exiting: I/O Error in poincare_driver.f90 (2)'
-  !       Endif
-  !       Write(99,*) nstart_fl
-
-  !       Do i = 1,nstart_fl
-  !          psiNout = get_psiN_2d(bfield,fl_r2(i,:),fl_z2(i,:),nsteps+1,ierr)
-  !          Where (psiNout < 1.e-3) psiNout = 1000000.d0
-  !          Write(99,*) Minval(psiNout)
-  !       Enddo
-
-  !       Close(99)
-  !       Deallocate(psiout,psiNout)
-  !    Endif
-  ! Endif
-
-
-  ! Deallocate(ilg,fl_ierr,fl_r,fl_z,fl_p,phistart_arr)
-
-  ! If (follow_both_ways) Then
-  !    Deallocate(ilg2,fl_ierr2,fl_r2,fl_z2,fl_p2)
-  ! Endif
-
-  ! Call Etime(tarray,tres)
-  ! Write(*,*) ' Poincare_driver took ',tres-tres0,' seconds'
-
 
 End Program poincare_driver
-
-
-
-
-
--- src/poincare/poincare_namelist.f90
+++ src/poincare/poincare_namelist.f90
@@ -2,38 +2,40 @@ Module poincare_namelist
   Use kind_mod, Only : real64, int32
   Implicit None
 
-  Real(real64) :: period, dphi_line_poincare
+  Real(real64) :: period, dphi_line_poincare, dphi_slice
   Integer(int32) :: ind_poin, nsteps
   
   ! Namelist variables:
   Real(real64) :: phistart_deg_poincare, rstart_poincare, rend_poincare, zstart_poincare, zend_poincare, dphi_line_deg_poincare
-  Integer(int32) :: num_pts, ntransits
+  Integer(int32) :: num_surfs, ntransits, num_slices, num_points_per_surf
   Logical :: follow_both_ways, quiet_bfield
   
   ! Namelist files
   Namelist / poincare_settings_nml / phistart_deg_poincare, Rstart_poincare, Rend_poincare, &
-       Zstart_poincare, Zend_poincare, num_pts, ntransits, dphi_line_deg_poincare, follow_both_ways, quiet_bfield
+       Zstart_poincare, Zend_poincare, num_surfs, ntransits, dphi_line_deg_poincare, follow_both_ways, &
+       num_slices, quiet_bfield
 
 Contains
 
   Subroutine read_poincare_namelist(verbose)
-    Use parallel_mod, Only : fin_mpi, rank, nprocs
+    Use parallel_mod, Only : fin_mpi, rank
     Use setup_bfield_module, Only : bfield_nml, nfp_bfield
     Use bfield_module, Only : verbose_bfield
     Use phys_const, Only : pi    
     Implicit None
     Logical, Intent(In) :: verbose
     Character(len=256) :: iomsg
-    Integer(int32) :: iocheck
+    Integer(int32) :: iocheck, i
     Real(real64) :: Adphirat
 
     ! --------------------------------
     ! Defaults
     ! --------------------------------
-    num_pts = 2
+    num_surfs = 2
     ntransits = 1
     follow_both_ways = .false.
     quiet_bfield = .true.
+    num_slices = 1
 
     ! -------------------------------
     ! Read settings namelists
@@ -74,25 +76,42 @@ Contains
 
     ! Step size in radians
     dphi_line_poincare = dphi_line_deg_poincare*pi/180.d0
+    
     ! Number of steps to complete the required number of transits. ntransits is full toroidal transits.    
     nsteps = Floor(ntransits*2.d0*pi/Abs(dphi_line_poincare))
-    ! Check that step size fits into the symmetric toroidal angle
-    Adphirat = Abs(360.d0/dphi_line_deg_poincare/nfp_bfield)
+    
+    ! Check that step size fits into the symmetric toroidal angle, --> also account for slices
+    Adphirat = Abs(360.d0/dphi_line_deg_poincare/Real(nfp_bfield,real64)/Real(num_slices,real64))
     If (Adphirat - Real(Nint(Adphirat)) > 1.d-8) Then
-       Write(*,*) 'Error!: 2*pi/nfp_bfield must be an integer multiple of dphi_line_deg'
+       Write(*,*) 'Error!: 2*pi/nfp_bfield/num_slices must be an integer multiple of dphi_line_deg'
        Write(*,*) 'Exiting'
        Call fin_mpi(.true.)
     Endif
-    ind_poin = Nint(Adphirat)
-    
-    Write(*,'(/1a,i0,a,i0,a)') 'Following ',num_pts,' fls for ',ntransits,' transits.'
-    Write(*,'(a,f12.3,a)') 'Toroidal step size is ',dphi_line_deg_poincare,' degrees'
-    Write(*,'(a,i0)') 'Poincare plot step size index is ',ind_poin
-    Write(*,'(a,f12.3)') 'Starting fl at phi = ',phistart_deg_poincare
-    Write(*,'(a,i0)') 'Number of steps = ',nsteps
-    
-    
-    
+
+    ! This is the stride required to stay at constant toroidal angle
+    ind_poin = Nint(Adphirat*num_slices)
+
+    ! Step size between slices
+    dphi_slice = 2.d0*pi/Real(nfp_bfield,real64)/Real(num_slices,real64)
+
+    ! Number of points returns per surface per slice    
+    num_points_per_surf = ntransits*nfp_bfield
+
+    If (verbose) Then
+       Write(*,'(/1a,i0,a)')      'Fieldlines are traced for ',ntransits,' transits.'    
+       Write(*,'(1a,i0,a,i0,a)')  'Tracing ',num_surfs,' surfaces with ',nsteps,' steps'
+       Write(*,'(a,g0.3,a)')      'Toroidal step size for tracing is ',dphi_line_deg_poincare,' degrees.'
+       Write(*,'(a,i0,a,i0,a)')   'With nfp = ',nfp_bfield,' there will be ~',num_points_per_surf,' points per surface'
+       Write(*,'(a,i0)')          'Stride for poincare plot is ',ind_poin
+       Write(*,'(a,i0,a,i0,a)')   'Saving data for ',num_surfs,' surfaces with ',num_slices,' slices (toroidal angles)'
+       If (num_slices .lt. 20) Then
+          Write(*,'(a,g0.3,a)')      'Toroidal angles for slices with dphi_slice = ',dphi_slice*180.d0/pi,' degrees'       
+          Do i = 0,num_slices-1
+             Write(*,'(f12.3)') dphi_slice*i*180.d0/pi
+          End Do
+       End If
+    End If
+        
   End Subroutine read_poincare_namelist
 
 End Module poincare_namelist
